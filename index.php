534ddc65a5148d05b12a942862b4a9aca5d3983c
Loc Bui <minhloc.vhit@gmail.com>
2018-08-02 23:25:48 +0700

    add route, db config

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-require "vendor/autoload.php";
+define('APP_URL', 'http://localhost');
+define('WEBROOT', APP_URL . '/app/views/assets');
+define('ROOT', str_replace("/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+define('VIEW', ROOT . '/App/Views/');
+
+require __DIR__ . "/vendor/autoload.php";
+require "route.php";
