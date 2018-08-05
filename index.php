0da1eaebee8ebe8945963d6c2677379bd816582a
Loc Bui <minhloc.vhit@gmail.com>
2018-08-05 10:48:12 +0700

    config phpunit test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_URL', 'http://mk.me.local');
+define('APP_URL', 'http://localhost');
 define('WEBROOT', APP_URL . '/app/views/assets');
 define('ROOT', str_replace("/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
 define('VIEW', ROOT . '/App/Views/');
